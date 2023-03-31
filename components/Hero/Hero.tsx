99fa8fa7c5c6db9921c018196240fa8134bf061a
Aleksander Smirnov <aleksander_smirnov@epam.com>
2023-03-31 23:28:03 +0300

    Hero component markup upgrade

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,97 +1,87 @@
 import React from "react";
 import { Box, Container, Typography, Button } from "@mui/material";
-import useHeroStyles from "./styles";
-import ButtonGroup from "@mui/material/ButtonGroup";
 import PlaylistAddCheckOutlinedIcon from "@mui/icons-material/PlaylistAddCheckOutlined";
 import ImageOutlinedIcon from "@mui/icons-material/ImageOutlined";
 import VerifiedIcon from "@mui/icons-material/Verified";
+import useHeroStyles from "./styles";
 
 const Hero = () => {
-  const { heroCSS } = useHeroStyles();
-
-  const IconWithText = ({ icon, text }) => {
-    return (
-      <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-        {icon}
-        <Box>{text}</Box>
-      </Box>
-    );
-  };
-
-  const onButtonClick1 = () => {
-    // код обработчика нажатия кнопки
-  };
-
-  const onButtonClick2 = () => {
-    // код обработчика нажатия кнопки
-  };
+  const { heroCSS, heroContainerCSS } = useHeroStyles();
 
   return (
-    <Box
-      component="section"
-      id="heroScreen"
-      sx={{
-        ...heroCSS,
-        backgroundImage: `url(/pictures.png)`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      <Container>
-        <Typography variant="h1" sx={{ mt: 10 }}>
-          Производство <br /> промышленных полов
-        </Typography>
-        <Typography variant="h2" sx={{ mt: 4 }}>
-          Мы обладаем значительным опытом работы в данной сфере и точно знаем,
-          чего <br /> ждет заказчик. Наши клиенты – это предприятия и
-          общественные организации, <br /> которые ценят свое время и деньги.
-        </Typography>
-
+    <Box component="section" id="heroScreen" sx={heroCSS}>
+      <Container maxWidth="md" sx={heroContainerCSS}>
         <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            p: 4,
-            pt: 49, // отступ сверху
-            pl: 0, // отступ слева
-            color: "white",
-            gap: 10,
-            marginLeft: "-120px", // отрицательный отступ слева
-            justifyContent: "flex-start", // align items to the left
-          }}
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          gap={4}
         >
-          <IconWithText
-            icon={<VerifiedIcon sx={{ color: "green" }} />}
-            text={<Typography variant="body1">Гарантия от 2 лет</Typography>}
-          />
-          <IconWithText
-            icon={<VerifiedIcon sx={{ color: "green" }} />}
-            text={<Typography variant="body1">Срок службы 20+ лет</Typography>}
-          />
-          <IconWithText
-            icon={<VerifiedIcon sx={{ color: "green" }} />}
-            text={<Typography variant="body1">Опыт работы 8+ лет</Typography>}
-          />
-        </Box>
+          <Typography variant="h1">Производство промышленных полов</Typography>
 
-        <ButtonGroup
-          variant="contained"
-          aria-label="outlined primary button group"
-          sx={{ position: "absolute", bottom: 200, left: 200 }}
-        >
-          <Button
-            sx={{ backgroundColor: "#EF4444", color: "white", mr: 1 }}
-            startIcon={<PlaylistAddCheckOutlinedIcon />}
-          >
-            Выбрать вид полов
-          </Button>
-          <Button
-            sx={{ backgroundColor: "#365FA1", color: "white" }}
-            startIcon={<ImageOutlinedIcon />}
-          >
-            Смотреть готовые работы
-          </Button>
-        </ButtonGroup>
+          <Typography variant="subtitle1">
+            Мы обладаем значительным опытом работы в данной сфере и точно знаем,
+            чего ждет заказчик. Наши клиенты – это предприятия и общественные
+            организации, которые ценят свое время и деньги.
+          </Typography>
+
+          <Box display="flex" alignItems="center" gap={4}>
+            <Box display="flex" gap={2} alignItems="center">
+              <VerifiedIcon color="success" fontSize="large" />
+              <Box>
+                <Typography variant="h4" component="p" fontWeight={400}>
+                  Гарантия
+                </Typography>
+                <Typography variant="h2" component="p">
+                  от 2 лет
+                </Typography>
+              </Box>
+            </Box>
+
+            <Box display="flex" gap={2} alignItems="center">
+              <VerifiedIcon color="success" fontSize="large" />
+              <Box>
+                <Typography variant="h4" component="p" fontWeight={400}>
+                  Срок службы
+                </Typography>
+                <Typography variant="h2" component="p">
+                  20+ лет
+                </Typography>
+              </Box>
+            </Box>
+
+            <Box display="flex" gap={2} alignItems="center">
+              <VerifiedIcon color="success" fontSize="large" />
+              <Box>
+                <Typography variant="h4" component="p" fontWeight={400}>
+                  Опыт работы
+                </Typography>
+                <Typography variant="h2" component="p">
+                  8+ лет
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+
+          <Box display="flex" alignItems="center" gap={2}>
+            <Button
+              variant="contained"
+              color="error"
+              size="large"
+              startIcon={<PlaylistAddCheckOutlinedIcon />}
+            >
+              Выбрать вид полов
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              startIcon={<ImageOutlinedIcon />}
+            >
+              Смотреть готовые работы
+            </Button>
+          </Box>
+        </Box>
       </Container>
     </Box>
   );
--- components/Hero/styles.ts
+++ components/Hero/styles.ts
@@ -2,43 +2,27 @@ import { SxProps, useTheme } from "@mui/material/styles";
 function useHeroStyles() {
   const theme = useTheme();
 
-  
-
   const heroCSS: SxProps = {
     display: "flex",
     flexDirection: "column",
     gap: theme.spacing(1),
-    p: theme.spacing(4),
     height: "calc(100vh - 80px - 36px)",
-    boxSizing: "border-box",
-    backgroundColor: theme.palette.grey[900],
-    textAlign: "center",
-    fontFamily: "Montserrat, sans-serif",
-    h1: {
-      fontSize: 48,
-      fontWeight: 700,
-      color: theme.palette.common.white,
-      textTransform: "uppercase",
-      position: "absolute",
-      top: 226, // Расстояние от верхнего края контейнера
-      left: 200, // Расстояние от левого края контейнера
-      textAlign: "left", // Выравнивание текста по левому краю
+    color: theme.palette.common.white,
+    backgroundImage: `url(/pictures.png)`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
+
+  const heroContainerCSS = {
+    height: "100%",
+    "& > *": {
+      height: "100%",
     },
-    h2: {
-      fontSize: 18,
-      fontWeight: 400,
-      color: theme.palette.common.white,
-      textTransform: "uppercase",
-      textAlign: "left", // Выравнивание текста по левому краю
-      position: "absolute", // Абсолютное позиционирование
-      top: 400, // Расстояние от верхнего края контейнера (при необходимости измените значение)
-      left: 200, // Расстояние от левого края контейнера (при необходимости измените значение)
-    },
-  
   };
 
   return {
     heroCSS,
+    heroContainerCSS,
   };
 }
 
